a523e607980f94d98876eafa9afb250c227d83a9
Pannry <thalestnd@gmail.com>
2016-09-13 22:19:47 -0300

    Botao mostrar esconder parede

---
+++ Projeto_Casa/Assets/Scripts/mostrarParedes.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class mostrarParedes : MonoBehaviour {
+
+	private bool opt;
+
+	public GameObject walls;
+
+	public void clickTest() {
+		if (opt == true) {
+			walls.GetComponent<Renderer>().enabled = !walls.GetComponent<Renderer>().enabled;
+			opt = !opt;
+			Debug.Log (opt);
+		} else {
+			walls.GetComponent<Renderer>().enabled = !walls.GetComponent<Renderer>().enabled;
+			opt = !opt;
+			Debug.Log (opt);
+		}
+	}
+
+}
+
+
+/*
+
+	void Update() {
+
+		if (Input.GetKeyDown(KeyCode.Z)) {
+			// show
+			// renderer.enabled = true;
+			walls.GetComponent<Renderer>().enabled = true;
+			Debug.Log ("z press!");
+		}
+
+		if (Input.GetKeyDown(KeyCode.X)) {
+			// hide
+			// renderer.enabled = false;
+			walls.GetComponent<Renderer>().enabled = false;
+
+			Debug.Log ("x press!");
+		}
+
+	}
+
+
+*/
