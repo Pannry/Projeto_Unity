d2e2fdb45efe7288203da6ef817841809a807bc4
Clovis <clovisdanielss@gmail.com>
2016-10-05 13:21:16 -0300

    Comentado codigo e ageitado uma porrada de coisa...

---
+++ Projeto_Casa/Assets/Scripts/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Node : MonoBehaviour
+{
+	private GameObject objeto;
+	private string rotulo;
+	public Node (GameObject objeto, string rotulo)
+	{
+		this.objeto = objeto;
+		this.rotulo = rotulo;
+	}
+	public GameObject GetObject(){
+		return objeto;
+	}
+	public string GetLabel(){
+		return rotulo;
+	}
+	public bool Compare(GameObject outro){
+		return outro.Equals (objeto);
+	}
+	public static bool isNode(string tag){
+		return (tag == Tags.EmbutidaLaje() || tag == Tags.QuadroEletrico());
+	}
+}
+
+
