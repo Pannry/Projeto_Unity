a6a834e7a0138cd5d6ea34838e4b0f4b41bb5149
Pannry <thalestnd@gmail.com>
2016-10-05 12:08:01 -0300

    aidicionado camera com movimento, uso das setas do teclado

--- Projeto_Casa/Assets/Scripts/ChangeCameras.cs
+++ Projeto_Casa/Assets/Scripts/ChangeCameras.cs
@@ -8,9 +8,16 @@ public class ChangeCameras : MonoBehaviour {
 	public void CamOneMove(){
 		cams [0].enabled = true;
 		cams [1].enabled = false;
+		cams [2].enabled = false;
 	}
 	public void CamTwoMove(){
 		cams [0].enabled = false;
 		cams [1].enabled = true;
+		cams [2].enabled = false;
+	}
+	public void CamThreeMove(){
+		cams [0].enabled = false;
+		cams [1].enabled = false;
+		cams [2].enabled = true;
 	}
 }
