16a793817ce4a8f7e79df10b57857e6cd22a4ced
Thales <thalestnd@gmail.com>
2017-01-24 16:39:06 -0300

    Mudando layout do menu
    Alterando o layout do menu, adicionando um painel para os objetos

---
+++ Projeto_Casa/Assets/Scripts/PopupObjetos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupObjetos : MonoBehaviour {
+
+	public Canvas Canvas_subMenu;
+	public bool subMenuOpen = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void SubMenuOpen(){
+		if (subMenuOpen == false){
+			subMenuOpen = true;
+			Canvas_subMenu.enabled = true;
+		}
+		else if (subMenuOpen == true){
+			subMenuOpen = false;
+			Canvas_subMenu.enabled = false;
+		}
+	}
+
+}
